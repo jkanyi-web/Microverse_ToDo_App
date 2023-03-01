c42e2d5f2aabda1af351b8920a38b590c29a08cf
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-03-01 18:45:28 +0300

    Add-remove functionality

--- src/index.js
+++ src/index.js
@@ -1,37 +1,93 @@
 /** @format */
 import './style.css';
 
-const listContainer = document.querySelector('.list');
+const USER_INPUT = document.getElementById('input');
+const ITEMS_CONTAINER = document.querySelector('.items');
+const ITEM_TEMPLATE = document.querySelector('.itemTemplate');
 
-const tasks = [
-  {
-    description: 'Cook',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'complete project',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'rest',
-    completed: false,
-    index: 3,
-  },
-];
+function getItems() {
+  const value = localStorage.getItem('todos') || '[]';
 
-tasks.sort((a, b) => a.index - b.index);
+  return JSON.parse(value);
+}
 
-tasks.forEach((task) => {
-  const li = document.createElement('li');
-  li.classList.add('task');
-  li.innerHTML = `
-        <label for="${task.index}">
-          <input type="checkbox" name="checkbox" id="${task.index}">
-          <p>${task.description}</p>
-        </label>
-        <i class="fa-solid fa-trash"></i>      
-  `;
-  listContainer.append(li);
+const items = getItems();
+
+function setItems() {
+  const itemsJson = JSON.stringify(items);
+  localStorage.setItem('todos', itemsJson);
+}
+
+function addItems() {
+  items.push({
+    description: USER_INPUT.value,
+    completed: false,
+    index: items.length + 1,
+  });
+
+  setItems();
+  // eslint-disable-next-line no-use-before-define
+  refreshList();
+}
+
+function updateItem(item, key, value) {
+  item[key] = value;
+  setItems(items);
+  // eslint-disable-next-line no-use-before-define
+  refreshList();
+}
+
+function refreshList() {
+  // sort items array
+  items.sort((a, b) => a.index - b.index);
+
+  ITEMS_CONTAINER.innerHTML = '';
+
+  items.forEach((item) => {
+    const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
+    const descriptionInput = itemElement.querySelector('.item-description');
+    const completedInput = itemElement.querySelector('.input');
+
+    descriptionInput.textContent = item.description;
+    completedInput.checked = item.completed;
+
+    descriptionInput.addEventListener('change', () => {
+      updateItem(item, 'description', item.description);
+    });
+
+    completedInput.addEventListener('change', () => {
+      updateItem(item, 'completed', completedInput.checked);
+    });
+
+    ITEMS_CONTAINER.append(itemElement);
+  });
+}
+
+refreshList();
+
+function removeItem() {
+  const deleteButtons = document.querySelectorAll('.task i');
+  deleteButtons.forEach((btn, id) => {
+    btn.addEventListener('click', (e) => {
+      items.splice(id, 1);
+      e.target.parentElement.remove();
+      setItems();
+    });
+    btn.index = id + 1;
+    setItems();
+  });
+}
+
+removeItem();
+
+function clearInput() {
+  USER_INPUT.value = '';
+}
+
+USER_INPUT.addEventListener('keyup', (e) => {
+  const userValue = USER_INPUT.value.trim();
+  if (e.key === 'Enter' && userValue) {
+    addItems();
+    clearInput();
+  }
 });
